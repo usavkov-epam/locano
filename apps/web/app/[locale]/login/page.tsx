a27fb912ad6e6cfab10d35cfe83bf51f26411ed8
Yury Saukou <yury_saukou@epam.com>
2025-06-30 21:37:05 +0400

    add login form

---
+++ apps/web/app/[locale]/login/page.tsx
@@ -0,0 +1,9 @@
+import { LoginForm } from "@locano/ui/components";
+
+export default function Page() {
+  return (
+    <div className="flex items-center justify-center min-h-svh">
+      <LoginForm />
+    </div>
+  )
+}
--- packages/ui/src/components/index.tsx
+++ packages/ui/src/components/index.tsx
@@ -1,1 +1,5 @@
 export * from './button';
+export * from './card';
+export * from './input';
+export * from './label';
+export * from './login-form';
